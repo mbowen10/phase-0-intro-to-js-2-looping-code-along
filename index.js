0a5414a51d71358f33dda18f282eb44f91fa5615
Michael Bowen <michaelbowen1092@gmail.com>
2022-01-26 14:27:06 -0500

    commit tree d450c50078c58742e0de9887f2ca09072feb2d43

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts){
+    for (let i=0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+const cards = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(cards){
+    for (let i = 0; i < cards.length; i++){
+        //console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return cards;
+}
